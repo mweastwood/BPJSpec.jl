dcd9fa06c786c46a137655be6b6b1ca7053ce21a
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-07-13 18:18:59 -0700

    Store more information in ObsParam
    Former-commit-id: 4dc3c6e7b032c7d61c09b136e96a831bff85faa3

--- src/obs.jl
+++ src/obs.jl
@@ -18,28 +18,47 @@ This is a container that stores some basic information about
 the interferometer and thed data being processed.
 """ ->
 immutable ObsParam
-    freq::Vector{Float64}
+    freq::Vector{Float64}        # list of frequency channels
+    antpos::Matrix{Float64}      # 3 by Nant array containing the (x,y,z) ITRF positions of each antenna
+    phasecenter::Vector{Float64} # unit vector specifying the ITRF phase center
     Nfreq::Int
     Nant::Int
     Nbase::Int
     Ntime::Int
 end
 
-function ObsParam(ms::Table,mmax)
+function ObsParam(ms::Table;mmax::Int=100)
     # Populate the observational parameters from the information
     # attached to a measurement set and the desired mmax to use
     # in m-mode analysis.
     lock(ms)
+    Ntime = 2mmax+1
+
+    # Frequency channels
     freq  = MeasurementSets.frequency(ms)
     Nfreq = length(freq)
-    Nant  = numrows(Table(ms[kw"ANTENNA"]))
+
+    # Antenna positions
+    antenna_table = Table(ms[kw"ANTENNA"])
+    antpos = antenna_table["POSITION"]
+    Nant = size(antpos,2)
     Nbase = div(Nant*(Nant-1),2) # no autocorrelations!
-    Ntime = 2mmax+1
+
+    # Phase center
+    frame = ReferenceFrame()
+    set!(frame,MeasurementSets.time(ms))
+    set!(frame,Measures.from_xyz_in_meters(Measures.ITRF,antpos[1,1],antpos[2,1],antpos[3,1]))
+    phasecenter_J2000 = MeasurementSets.phase_direction(ms)
+    phasecenter_ITRF  = measure(frame,phasecenter_J2000,Measures.ITRFDIR)
+    phasecenter = Measures.xyz_in_meters(phasecenter_ITRF)
+
     unlock(ms)
-    ObsParam(freq,Nfreq,Nant,Nbase,Ntime)
+    ObsParam(freq,antpos,phasecenter,Nfreq,Nant,Nbase,Ntime)
 end
 
 freq(obs::ObsParam) = obs.freq
+antpos(obs::ObsParam) = obs.antpos
+phasecenter(obs::ObsParam) = obs.phasecenter
 Nfreq(obs::ObsParam) = obs.Nfreq
 Nant(obs::ObsParam) = obs.Nant
 Nbase(obs::ObsParam) = obs.Nbase
