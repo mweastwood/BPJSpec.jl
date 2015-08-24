3e51a063cf9ff2c983a3037f9b13ac387b9dc9e6
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-08-24 14:51:34 -0700

    importall Base.Operators and __precompile__()

--- src/BPJSpec.jl
+++ src/BPJSpec.jl
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+__precompile__()
+
 module BPJSpec
 
 export TransferMatrix, MModes
@@ -30,6 +32,7 @@ using CasaCore.Measures
 using CasaCore.Tables
 using MeasurementSets
 
+importall Base.Operators
 import Base: touch
 import HEALPix: Alm, lmax, mmax
 
