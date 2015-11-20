fab7ef4e4458b0ad4d853330f670a4bc8c035efb
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-11-19 16:42:53 -0800

    remove out-of-date test of SpectralTransferMatrix and SpectralMModes

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,26 +18,3 @@ include("mmodes.jl")
 include("alm.jl")
 include("covariancematrix.jl")
 
-#=
-# Test SpectralMModes
-let
-    mmax = 10
-    a = BPJSpec.MModeBlock{mmax}(0,complex(randn(5),randn(5)))
-    b = BPJSpec.MModeBlock{mmax}(0,complex(randn(5),randn(5)))
-    v = SpectralMModes{mmax}(0,[a,b])
-    @test full(v) == [a.block; b.block]
-end
-
-# Test SpectralTransferMatrix
-let
-    lmax = 10
-    mmax = 10
-    a = BPJSpec.TransferMatrixBlock{lmax,mmax}(0,complex(randn(5,5),randn(5,5)))
-    b = BPJSpec.TransferMatrixBlock{lmax,mmax}(0,complex(randn(5,5),randn(5,5)))
-    z = zeros(Complex128,5,5)
-    B = SpectralTransferMatrix{lmax,mmax}(0,[a,b])
-    @test full(B) == [a.block z;
-                      z b.block]
-end
-=#
-
