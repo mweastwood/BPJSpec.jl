2626cf7bb502d907041316710c25fa2a4f6d2bce
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-11-22 23:26:39 -0800

    test tikhonov

--- test/alm.jl
+++ test/alm.jl
@@ -10,3 +10,20 @@ let lmax = 20, mmax = 10
     end
 end
 
+let lmax = 20, mmax = 10, Nbase = 100, ν = 45e6
+    B = TransferMatrix(Nbase,lmax,mmax,ν)
+    for m = 0:mmax
+        rand!(B[m].block)
+    end
+
+    alm = Alm(Complex128,lmax,mmax)
+    rand!(alm.alm)
+    v = B*alm
+
+    alm′ = tikhonov(B,v,tolerance=0.0)
+    @test alm.alm ≈ alm′.alm
+
+    alm′ = tikhonov(B,v,tolerance=1e16)
+    @test vecnorm(alm′.alm) < 1e-10
+end
+
