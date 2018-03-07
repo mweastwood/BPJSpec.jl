d157fdbb9d74bee7fab2ed3c7bd2084ec42bfa36
autodocs <autodocs>
2018-03-07 18:13:37 +0000

    build based on 8566822

--- latest/search_index.js
+++ latest/search_index.js
@@ -12,7 +12,7 @@ var documenterSearchIndex = {"docs": [
     "location": "#BPJSpec.HierarchicalTransferMatrix",
     "page": "Home",
     "title": "BPJSpec.HierarchicalTransferMatrix",
-    "category": "Type",
+    "category": "type",
     "text": "struct HierarchicalTransferMatrix\n\nThis type represents the transfer matrix of an interferometer. This matrix effectively describes how an interferometer responds to the sky, including the antenna primary beam, bandpass, and baseline distribution.\n\n\"Hierarchical\" refers to the fact that we save on some computational and storage requirements by separating long baselines from short baselines.\n\nFields\n\npath points to the directory where the matrix is stored\nmetadata describes the properties of the interferometer\nhierarchy describes how the baselines are grouped\nfrequencies is an alias for metadata.frequencies\nbandwidth is an alias for metadata.bandwidth\nlmax is the maximum value of the total angular momentum quantum number l\nmmax is the maximum value of the azimuthal quantum number m\n\nImplementation\n\nAll of the data is stored on disk and only read into memory on-request. Generally, this approach is necessary because the entire transfer matrix is too large to entirely fit in memory, and because the matrix is block diagonal we can work with blocks individually.\n\n\n\n"
 },
 
